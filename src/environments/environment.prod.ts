2d832a9aa050c7573d29a2f72258c1ff60418dc7
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2022-11-07 20:19:25 -0600

    switches serverurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://employee-manager-springboot.herokuapp.com/api'
+  //serverUrl: 'https://employee-manager-springboot.herokuapp.com/api'
+  serverUrl: 'https://backendecs.herokuapp.com/api'
 };
